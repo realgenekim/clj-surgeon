dfaef0b11d14e40053ffb1a58d10fab13864f3cb
forge-anvil <forge-anvil@anvil>
2026-09-10 21:42:20 +0000

    Add RED witness: insert_forms_after_prefix_named_defn_test

---
+++ test/clj_surgeon/insert_forms_after_prefix_named_defn_test.clj
@@ -0,0 +1,18 @@
+(ns clj-surgeon.insert-forms-after-prefix-named-defn-test
+  {:lane :fast}
+  (:require
+   [clj-surgeon.insert-forms :as insert]
+   [clj-surgeon.insert-forms-support :as h]
+   [clojure.java.io :as io]
+   [clojure.test :refer [deftest is testing]])
+  (:import
+   (java.nio.file Files)))
+
+;; @spec INSERT-FORMS-001
+;; INTENT-TEST: INSERT-FORMS-001
+(deftest insert-forms-after-prefix-named-defn
+
+  (h/accepted h/source (h/request h/source)
+              "(defn a [] 1)\n(defn b [] 3)\n\n(defn ab [] 2)\n")
+  (h/accepted h/source (assoc-in (h/request h/source) [:anchor :position] "before")
+              "(defn b [] 3)\n(defn a [] 1)\n(defn ab [] 2)\n"))
