f389fd8bc08f767e8cac51b76ca73fc18a62457d
forge-anvil <forge-anvil@anvil>
2026-09-10 22:24:12 +0000

    test: finish unbalanced payload witness coverage

--- test/clj_surgeon/insert_forms_unbalanced_payload_test.clj
+++ test/clj_surgeon/insert_forms_unbalanced_payload_test.clj
@@ -1,12 +1,8 @@
 (ns clj-surgeon.insert-forms-unbalanced-payload-test
   {:lane :fast}
   (:require
-   [clj-surgeon.insert-forms :as insert]
    [clj-surgeon.insert-forms-support :as h]
-   [clojure.java.io :as io]
-   [clojure.test :refer [deftest is testing]])
-  (:import
-   (java.nio.file Files)))
+   [clojure.test :refer [deftest]]))
 
 ;; @spec INSERT-FORMS-003
 ;; INTENT-TEST: INSERT-FORMS-003
