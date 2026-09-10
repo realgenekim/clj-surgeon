73014b277bdafc989ec7c7afad0a0a5ac884c4db
forge-anvil <forge-anvil@anvil>
2026-09-10 21:42:20 +0000

    Add RED witness: insert_forms_anchor_decoys_test

---
+++ test/clj_surgeon/insert_forms_anchor_decoys_test.clj
@@ -0,0 +1,20 @@
+(ns clj-surgeon.insert-forms-anchor-decoys-test
+  {:lane :fast}
+  (:require
+   [clj-surgeon.insert-forms :as insert]
+   [clj-surgeon.insert-forms-support :as h]
+   [clojure.java.io :as io]
+   [clojure.test :refer [deftest is testing]])
+  (:import
+   (java.nio.file Files)))
+
+;; @spec INSERT-FORMS-005
+;; INTENT-TEST: INSERT-FORMS-005
+(deftest insert-forms-comment-string-anchor-decoys
+
+  (doseq [decoy ["; (defn a [] 1)\n" "\"(defn a [] 1)\"\n"
+                 "(comment (defn a [] 1))\n" "'(defn a [] 1)\n" "#_(defn a [] 1)\n"]]
+    (h/refused decoy (h/request decoy) :anchor-not-found)
+    (let [s (str decoy h/source)]
+      (h/accepted s (h/request s)
+                  (str decoy "(defn a [] 1)\n(defn b [] 3)\n\n(defn ab [] 2)\n")))))
