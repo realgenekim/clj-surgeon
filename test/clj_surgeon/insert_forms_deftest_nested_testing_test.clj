e7f235579fbf261b2cfcd11a28c948d3e98e59d0
forge-anvil <forge-anvil@anvil>
2026-09-10 22:24:12 +0000

    test: finish deftest nested testing witness coverage

--- test/clj_surgeon/insert_forms_deftest_nested_testing_test.clj
+++ test/clj_surgeon/insert_forms_deftest_nested_testing_test.clj
@@ -1,17 +1,20 @@
 (ns clj-surgeon.insert-forms-deftest-nested-testing-test
   {:lane :fast}
   (:require
-   [clj-surgeon.insert-forms :as insert]
    [clj-surgeon.insert-forms-support :as h]
-   [clojure.java.io :as io]
-   [clojure.test :refer [deftest is testing]])
-  (:import
-   (java.nio.file Files)))
+   [clojure.test :refer [deftest]]))
 
 ;; @spec INSERT-FORMS-002
 ;; INTENT-TEST: INSERT-FORMS-002
 (deftest insert-forms-deftest-nested-testing
 
+  (let [f (h/fixtures) s (:test-text f)
+        req (assoc (h/request s)
+                   :anchor (assoc (h/body "deftest" "admitted" {:position "last"})
+                                  :testing_path [{:label (:outer-label f) :expect 1}
+                                                 {:label (:inner-label f) :expect 1}])
+                   :payload {:text "(is (= 1 1))" :forms 1})]
+    (h/accepted s req (str (subs s 0 (- (count s) 3)) "\n      (is (= 1 1))\n)))")))
   (let [s "(deftest t\n  (testing \"outer\"\n    (testing \"inner\"\n      (is true)))\n  (testing \"other\" (testing \"inner\" (is false))))"
         a (assoc (h/body "deftest" "t" {:position "last"})
                  :testing_path [{:label "outer" :expect 1} {:label "inner" :expect 1}])
